1116f03ce28b540dac74da3082bfef89c84d1182
Avi496 <78342708+Avi496@users.noreply.github.com>
2021-08-13 17:17:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(350, 300);
+    canvas.center();
+}
+function draw(){
+    fill(200, 167, 254);
+    stroke(200, 167, 254);
+    circle(10, 10, 20);
+    fill(129, 251, 30);
+    stroke(129, 251, 30);
+    ellipse(100, 50, 100, 55);
+    fill(221, 67, 90);
+    stroke(221, 67, 90);
+    rect(200, 200, 80, 50);
+}
